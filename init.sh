cfdfbee21dc476811319a3b28aa9c427ff9b52fc
rui tao <5239604@qq.com>
2019-12-13 20:58:36 +0800

    python3.8virtualenv virtualenvwrapper安装配置

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 echo "编译安装python3"
 echo "安装依赖"
 sudo apt-get update 
-sudo apt-get install -y wget build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+sudo apt-get install -y wget build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev gcc
 echo "下载python3.8的压缩包"
 wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
 echo "解压压缩包"
@@ -17,4 +17,5 @@ echo "把python3.8的bin目录加入到环境变量"
 echo "export PATH=/opt/python38/bin:$PATH" >> /etc/profile
 source /etc/profile
 echo "安装Done..."
-
+echo "升级pip3"
+pip3 install --upgrade pip
